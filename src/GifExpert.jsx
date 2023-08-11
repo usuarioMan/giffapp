1464386bd2b8a674b4117d0d2f63f6d7028ace5a
usuarioman <castilla.ar@gmail.com>
2023-08-11 14:50:42 -0600

    add simple useState with an array and then parse the elements with a simple map function

--- src/GifExpert.jsx
+++ src/GifExpert.jsx
@@ -1,8 +1,14 @@
+import { useState } from "react"
+import { v4 as uuidv4 } from 'uuid';
 
 export const GifExpert = () => {
-  return (
-    <>
-        <h1>Gif Expert App</h1>
-    </>
-  )
+    const [categories, setCategories] = useState([ 'One Punch' , 'POTE'])
+    return (
+        <>
+            <h1>Gif Expert App</h1>
+            <ol>
+                {categories.map(category => <li key={uuidv4()}>{category}</li>)}
+            </ol>
+        </>
+    )
 }
