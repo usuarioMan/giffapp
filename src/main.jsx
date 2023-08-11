3fbd56f2eb34881398468e13220653c70a7e5fd4
usuarioman <castilla.ar@gmail.com>
2023-08-11 12:36:43 -0600

    First commit. Add .env variable to gitignore and create some files to start the app.

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { GifExpert } from './GifExpert'
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GifExpert/>
+  </React.StrictMode>,
+)
